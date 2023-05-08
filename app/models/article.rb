70d26c021138fb9d5d069bc2030e83faaa70d99c
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-08 22:11:30 +0700

    validation added in model file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: {minumum: 6, maximun: 100}
+    validates :description, presence: true, length {minimun: 10m maximun: 30}
 end
