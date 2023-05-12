103f02de666d846bf8e3fb21c8d07a6fa740aab5
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-13 01:01:55 +0700

    UI update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit', status: :unprocessable_entity
         end
