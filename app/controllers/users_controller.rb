7d145d973f28254baaf1ea139170527f51f49a52
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-13 15:56:22 +0700

    delete users and related articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
 
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and Aritcle deleted"
+        redirect_to root_path
     end
 
     private
